d39fd3a331cba3eb049cf10aa9abe871c8074304
Andrew <thebuffylow@gmail.com>
2022-11-13 14:13:38 -0600

    test two passed and save repos /add

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message = "Location is needed")
+    @Size(min = 3, max = 150, message = "Location must be between 3 and 100 characters")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+    public Employer() {}
 
 
+    public void setLocation(String saint_louis) {
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank(message = "Description is needed")
+    @Size(min = 3, max = 255, message = "Description must be between 3 and 300 characters")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
 
 }
